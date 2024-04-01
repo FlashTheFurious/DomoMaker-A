1c3a2e391875b83bfeb6c9b700a0636a5703b7a6
FlashTheFurious <53021141+FlashTheFurious@users.noreply.github.com>
2024-03-31 21:58:17 -0400

    p3

---
+++ server/middleware/index.js
@@ -0,0 +1,34 @@
+const requireLogin = (req, res, next) => {
+    if(req.session.account) {
+      return res.redirect('/');
+    }
+    return next();
+  }
+  
+  const requireLogout = (req, res, next) => {
+    if(req.session.account) {
+      return res.redirect('/maker');
+    }
+    return next();
+  }
+  
+  const requireSecure = (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(`https://${req.hostname}${req.url}`);
+    }
+    return next();
+  }
+  
+  const bypassSecure = (req, res, next) => {
+    next();
+  }
+  
+  module.exports.requiresLogin = requireLogin;
+  module.exports.requiresLogout = requireLogout;
+  
+  if (process.env.NODE_ENV === 'production') {
+    module.exports.requireSecure = requireSecure;
+  } else {
+    module.exports.requireSecure = bypassSecure;
+  }
+  
